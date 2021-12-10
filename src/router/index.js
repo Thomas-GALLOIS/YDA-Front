2507b01b31ef96e05db27409dee00d0b18f09479
JB <jeanbernard_doffemont@yahoo.fr>
2021-12-10 15:52:57 +0100

    commit NavBarre

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Connexion from "../views/Connexion.vue";
+import AddAccount from "../views/AddAccountByAdmin.vue";
 
 const routes = [
   {
@@ -21,6 +22,11 @@ const routes = [
     path: "/connexion",
     name: "Connexion",
     component: Connexion,
+  },
+  {
+    path: "/creation",
+    name: "AddAccount",
+    component: AddAccount,
   },
 ];
 
