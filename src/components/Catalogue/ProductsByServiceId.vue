4e9de3bf7e4ef9f6af438ffb37895b740d40a0a8
ELLIOT Valentin <elliot.valentin96@gmail.com>
2021-12-22 15:17:06 +0100

    commit

--- src/components/Catalogue/ProductsByServiceId.vue
+++ src/components/Catalogue/ProductsByServiceId.vue
@@ -59,7 +59,7 @@ export default {
         method: "POST",
 
         headers: {
-          Authorization: "Bearer " + localStorage.getItem("token"),
+          Authorization: "Bearer " + localStorage.getItem("@token"),
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
