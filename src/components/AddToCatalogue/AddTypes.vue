994a00d90f8807a4e3f12591f1d16ebba417259f
ELLIOT Valentin <elliot.valentin96@gmail.com>
2021-12-23 10:57:33 +0100

    commit

--- src/components/AddToCatalogue/AddTypes.vue
+++ src/components/AddToCatalogue/AddTypes.vue
@@ -50,7 +50,8 @@ export default {
 
       headers: {
         "Content-Type": "application/json",
-        Authorization: "bearer " + localStorage.getItem("token"),
+        Authorization: "Bearer " + localStorage.getItem("token"),
+        Accept: "application/json",
       },
     };
     const response = await fetch(url, options);
